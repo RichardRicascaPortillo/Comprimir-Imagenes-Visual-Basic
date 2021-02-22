bbf67999329f498db8ed65b272e778af042fb7f6
RichardRicascaPortillo <richardricasca@gmail.com>
2021-02-22 12:14:13 -0500

    Reduce peso en megabytes

---
+++ imagenesComprimirVisualBasic/Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnRuta = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.LabelProcesado = New System.Windows.Forms.Label()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.txRuta = New System.Windows.Forms.TextBox()
+        Me.txPrefijo = New System.Windows.Forms.TextBox()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnRuta
+        '
+        Me.btnRuta.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRuta.Location = New System.Drawing.Point(653, 245)
+        Me.btnRuta.Name = "btnRuta"
+        Me.btnRuta.Size = New System.Drawing.Size(121, 44)
+        Me.btnRuta.TabIndex = 0
+        Me.btnRuta.Text = "Examinar ..."
+        Me.btnRuta.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(653, 375)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(121, 47)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Comprimir"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(44, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 24)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Ruta"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(44, 98)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(193, 24)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Renombrar Imagenes"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(284, 228)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(195, 24)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Nivel de comprension"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(58, 375)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(101, 24)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Procesado"
+        '
+        'LabelProcesado
+        '
+        Me.LabelProcesado.AutoSize = True
+        Me.LabelProcesado.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelProcesado.Location = New System.Drawing.Point(200, 375)
+        Me.LabelProcesado.Name = "LabelProcesado"
+        Me.LabelProcesado.Size = New System.Drawing.Size(40, 24)
+        Me.LabelProcesado.TabIndex = 7
+        Me.LabelProcesado.Text = "0 %"
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(48, 169)
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(726, 56)
+        Me.TrackBar1.TabIndex = 9
+        '
+        'txRuta
+        '
+        Me.txRuta.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txRuta.Location = New System.Drawing.Point(129, 35)
+        Me.txRuta.Name = "txRuta"
+        Me.txRuta.Size = New System.Drawing.Size(645, 30)
+        Me.txRuta.TabIndex = 10
+        '
+        'txPrefijo
+        '
+        Me.txPrefijo.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txPrefijo.Location = New System.Drawing.Point(243, 98)
+        Me.txPrefijo.Name = "txPrefijo"
+        Me.txPrefijo.Size = New System.Drawing.Size(531, 30)
+        Me.txPrefijo.TabIndex = 11
+        '
+        'BackgroundWorker1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txPrefijo)
+        Me.Controls.Add(Me.txRuta)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.LabelProcesado)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.btnRuta)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnRuta As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents LabelProcesado As Label
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents txRuta As TextBox
+    Friend WithEvents txPrefijo As TextBox
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+End Class
---
+++ imagenesComprimirVisualBasic/Form1.vb
@@ -0,0 +1,107 @@
+﻿Imports System.ComponentModel
+Imports System.Drawing.Imaging
+Imports System.IO
+
+Public Class Form1
+    Dim directoryPath As String = ""
+    Dim nombreArchivo As String = ""
+    Dim valorComprension As Long = 0
+    Dim diferencia As Integer = 0
+    Dim restante As Integer = 0
+    Dim progreso As Integer = 0
+    Dim contador As Integer = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+    Private Sub btnprefijo_Click(sender As Object, e As EventArgs)
+        txRuta.Text = Directory.GetCurrentDirectory()
+    End Sub
+
+    Private Sub btnRuta_Click(sender As Object, e As EventArgs) Handles btnRuta.Click
+        If FolderBrowserDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            txRuta.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+    Private Function GetEncoder(ByVal format As ImageFormat) As ImageCodecInfo
+        Dim codecs As ImageCodecInfo() = ImageCodecInfo.GetImageEncoders()
+        Dim codec As ImageCodecInfo
+        For Each codec In codecs
+            If codec.FormatID = format.Guid Then
+                Return codec
+            End If
+        Next codec
+        Return Nothing
+    End Function
+    Private Sub VaryQualityLevel()
+        If txRuta.Text.Length > 1 Then
+            directoryPath = txRuta.Text
+
+            Dim Final As Integer = TrackBar1.Value & 0
+            valorComprension = (100 - Final)
+
+            BackgroundWorker1.WorkerSupportsCancellation = True
+            BackgroundWorker1.WorkerReportsProgress = True
+
+            BackgroundWorker1.RunWorkerAsync()
+        End If
+
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Dim files As String() = Directory.GetFiles(directoryPath, "*.JPG", SearchOption.AllDirectories)
+        Dim total As Integer = files.Length
+
+        For Each s As String In files
+            directoryPath = Path.GetDirectoryName(s)
+            nombreArchivo = Path.GetFileName(s)
+
+            Using bmp1 As New Bitmap(s)
+                Dim jpgEncoder As ImageCodecInfo = GetEncoder(ImageFormat.Jpeg)
+
+                Dim myEncoder As System.Drawing.Imaging.Encoder = System.Drawing.Imaging.Encoder.Quality
+                Dim myEncoderparameters As New EncoderParameters(1)
+
+                Dim myEncoderParameter As New EncoderParameter(myEncoder, valorComprension&)
+                myEncoderparameters.Param(0) = myEncoderParameter
+                bmp1.Save(directoryPath & "\" & txPrefijo.Text & nombreArchivo, jpgEncoder, myEncoderparameters)
+
+            End Using
+            contador = contador + 1
+            diferencia = total
+            restante = ((CInt(diferencia) - contador) / diferencia) * 100
+            progreso = 100 - restante
+
+            BackgroundWorker1.ReportProgress(CInt(progreso), "Running..." & progreso.ToString)
+
+
+
+
+        Next
+    End Sub
+
+    Private Sub BackgroundWorker1_ProgressChanged(sender As Object, e As ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+        LabelProcesado.Text = e.ProgressPercentage.ToString & " % Completado"
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        If e.Error IsNot Nothing Then
+            MessageBox.Show("Ocurrio un error", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        ElseIf e.Cancelled Then
+            MessageBox.Show("Tarea cancelada", "Atencion", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        Else
+            MessageBox.Show("Terminado", "Atencion", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If txPrefijo.Text.Length > 0 Then
+            VaryQualityLevel()
+        Else
+            MessageBox.Show("Ingrese texto en renombrar Imagenes", "Atencion", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End If
+    End Sub
+End Class
+
